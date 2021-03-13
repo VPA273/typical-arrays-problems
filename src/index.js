5e0ee69b60c7015d69e0f165ac69e4cf06ee302a
VPA273 <vpa273@gmail.com>
2021-03-13 15:36:15 +0300

    refactor: apply prettier

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array ) return 0;
+  if (array.length == 0 ) return 0;
+let m = array[0];
+for( let i=1 ; i < array.length ; i++ ) {
+  if (m > array[i] ) {m = array[i]}
+}
+  return (m);
+return 0
 }
 
 exports.max = function max (array) {
+  if (!array ) return 0;
+  if (array.length == 0) return 0;
+  let mx = array[0];
+  for( let i=1 ; i < array.length ; i++ ) {
+    if (mx < array[i] ) {mx = array[i]}
+  }
+    return (mx);
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array ) return 0;
+  if (array.length == 0 ) return 0;
+  let sum = 0;
+  for (item of array ) { sum += item }
+  return (sum / array.length );
+  return 0
 }
